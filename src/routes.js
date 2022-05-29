2d944e69c4fae00961d873d3710432e89ca43eca
Upadesh Bhatta <upadesh.ng@gmail.com>
2022-05-29 14:02:51 +1000

    student list route and payload store

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
+const StudentList = React.lazy(() => import('./views/student/List'))
 const StudentAdd = React.lazy(() => import('./views/student/Add'))
 const StudentUpdate = React.lazy(() => import('./views/student/Update'))
 const StudentView = React.lazy(() => import('./views/student/View'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
+  { path: '/student', name: 'Students', element: StudentList },
   { path: '/student/add', name: 'Student Add', element: StudentAdd },
   { path: '/student/update/:id', name: 'Student Update', element: StudentUpdate },
   { path: '/student/view/:id', name: 'Student Update', element: StudentView },
